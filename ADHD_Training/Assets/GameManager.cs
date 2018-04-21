4d6ffa6ce1df6bf9ef1c629ebd426b4485b8272c
Emilie Thoreson <thoresonemilie@gmail.com>
2018-04-21 10:38:05 -0700

    Basic true false logic backend done
    Basic true false logic backend done

---
+++ ADHD_Training/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour {
+	public Question[] questions;
+	private static List<Question> unansweredQuestions;
+	private Question currentQuestion;
+
+	void Start ()
+	{
+		if (unansweredQuestions == null || unansweredQuestions.Count == 0) 
+		{
+			unansweredQuestions = questions.ToList<Question>();
+		}
+
+		GetRandomQuestion ();
+		Debug.Log (currentQuestion.fact + " is " + currentQuestion.isTrue);
+	}
+
+	void GetRandomQuestion ()
+	{
+		int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+		currentQuestion = unansweredQuestions[randomQuestionIndex];
+		unansweredQuestions.RemoveAt(randomQuestionIndex);
+	}
+}
