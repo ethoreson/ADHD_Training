56b6477562dda9b2325dd5c84f1af9b668bb7c7c
Emilie Thoreson <thoresonemilie@gmail.com>
2018-04-21 14:51:24 -0700

    MVP of block game complete
    MVP of block game complete

---
+++ ADHD_Training/Assets/Scripts/onTouchBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THIS CLASS AND ITS ONTOUCH FUNCTION WILL BE IMPLEMENTED IN THE VR SOFTWARE.
+// IT DOES NOT SERVE A PURPOSE IN UNITY AND NEEDS TO BE CHANGED.
+
+public class onTouchBehavior : MonoBehaviour {
+
+	public static int grandTotalScore;
+
+	void onTouch() {
+		grandTotalScore += 1;
+	}
+}
