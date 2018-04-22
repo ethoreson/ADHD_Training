bd8bdd04f64e8fa27c299c3ba4f5fbec94aa9f86
haleh <haleh.bahadori@gamil.com>
2018-04-22 12:15:47 -0700

    final classroom changes

--- Assets/Standard Assets/CrossPlatformInput/Scripts/InputAxisScrollbar.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/InputAxisScrollbar.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 
@@ -16,22 +15,3 @@ namespace UnityStandardAssets.CrossPlatformInput
         }
     }
 }
-=======
-using System;
-using UnityEngine;
-
-namespace UnityStandardAssets.CrossPlatformInput
-{
-    public class InputAxisScrollbar : MonoBehaviour
-    {
-        public string axis;
-
-	    void Update() { }
-
-	    public void HandleInput(float value)
-        {
-            CrossPlatformInputManager.SetAxis(axis, (value*2f) - 1f);
-        }
-    }
-}
->>>>>>> 8459d98a5a0ef532ea6f483556e0488ececaf920
--- Assets/Standard Assets/Utility/FollowTarget.cs
+++ Assets/Standard Assets/Utility/FollowTarget.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 
@@ -17,23 +16,3 @@ namespace UnityStandardAssets.Utility
         }
     }
 }
-=======
-using System;
-using UnityEngine;
-
-
-namespace UnityStandardAssets.Utility
-{
-    public class FollowTarget : MonoBehaviour
-    {
-        public Transform target;
-        public Vector3 offset = new Vector3(0f, 7.5f, 0f);
-
-
-        private void LateUpdate()
-        {
-            transform.position = target.position + offset;
-        }
-    }
-}
->>>>>>> 8459d98a5a0ef532ea6f483556e0488ececaf920
