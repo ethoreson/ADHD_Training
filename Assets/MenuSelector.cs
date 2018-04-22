9aa7fc08b50db5de1664c5909c9ad687422ca512
Niveditha Kalavakonda <nkalavakonda@gmail.com>
2018-04-22 14:38:46 -0400

    Added Test Scripts

---
+++ Assets/MenuSelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using VRTK;
+
+public class MenuSelector : MonoBehaviour {
+	public VRTK_ControllerEvents controllerEvents;
+	public GameObject menu;
+	//bool menuState = false;
+
+	void OnEnable(){
+		//controllerEvents.buttonTwoPressed += controllerEvents_ButtonTwoPressed;
+		controllerEvents.TriggerClicked += controllerEvents_TriggerClicked;
+		Debug.Log ("Pointer Selection Made");
+	}
+
+	void OnDisable(){
+		//controllerEvents.buttonTwoPressed -= controllerEvents_ButtonTwoPressed;
+		controllerEvents.TriggerClicked -= controllerEvents_TriggerClicked;
+	}
+
+	private void controllerEvents_TriggerClicked(object sender, ControllerInteractionEventArgs e)
+	{
+		//menuState = !menuState;
+		//menu.SetActive (menuState);		//throw new System.NotImplementedException ();
+		Debug.Log("Section Executed");
+	}
+
+}
